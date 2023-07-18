ff4a0af258aaac66ce7ebce8502ebdcd0a1ddca0
rulewrite <rulewrite@gmail.com>
2023-07-18 21:21:59 +0900

    feat: 서버 사이드에서 user 목록 가져오기

--- ssr-rest-api/src/pages/index.tsx
+++ ssr-rest-api/src/pages/index.tsx
@@ -1,9 +1,16 @@
+import axios from 'axios';
 import Link from 'next/link';
 
-export const getServerSideProps = () => {
+export const getServerSideProps = async () => {
+  // const usersRequest = await axios.get('https://api.rwnjs.com/04/users');
+  // 책 예제가 동작하지 않으므로 jsonplaceholder로 대체
+  const usersRequest = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+
   return {
     props: {
-      users: [],
+      users: usersRequest.data,
     },
   };
 };
