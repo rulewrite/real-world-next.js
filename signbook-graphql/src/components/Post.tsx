67143719216708363a95049acc9fee243e71611b
rulewrite <rulewrite@gmail.com>
2023-07-18 23:54:53 +0900

    feat: Post 컴포넌트 추가

---
+++ signbook-graphql/src/components/Post.tsx
@@ -0,0 +1,15 @@
+const Post = ({ post }: any) => {
+  return (
+    <div className="max-w-7xl rounded-md border-2 border-purple-800 shadow-xl bg-purple-50 p-7 mb-10">
+      <p className="text-gray-700"> {post.body} </p>
+      <hr className="mt-3 mb-3 border-t-0 border-b-2 border-purple-800" />
+      <div>
+        <div className="text-purple-900">
+          Written by <b>{post.userId}</b>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- signbook-graphql/src/pages/index.tsx
+++ signbook-graphql/src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Loading from '@/components/Loading';
+import Post from '@/components/Post';
 import GET_POSTS from '@/lib/apollo/queries/getPosts';
 import { useQuery } from '@apollo/client';
+import Link from 'next/link';
 
 export default function Home() {
   const { loading, data } = useQuery(GET_POSTS, {
@@ -12,12 +14,18 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <ul>
-        {data.posts.map((post: any) => {
-          return <li key={post.id}>{post.title}</li>;
-        })}
-      </ul>
+    <div className="flex justify-center items-center flex-col mt-20">
+      <h1 className="text-3xl mb-5">Real-World Next.js signbook</h1>
+      <Link href="/new-sign">
+        <button className="mb-8 border-2 border-purple-800 text-purple-900 p-2 rounded-lg text-gray-50 m-auto mt-4">
+          Add new sign
+        </button>
+      </Link>
+      <div>
+        {data.posts.map((post: any) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   );
 }
