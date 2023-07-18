ddabb67443d519621dfd7d22ff2d3451bd008064
rulewrite <rulewrite@gmail.com>
2023-07-18 23:02:40 +0900

    feat: 아폴로 클라이언트 초기화 함수 정의

--- signbook-graphql/lib/apollo/index.ts
+++ signbook-graphql/lib/apollo/index.ts
@@ -1,11 +1,41 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
 
+const isSsr = typeof window === 'undefined';
 let uri = '/api/graphql';
+let apolloClient: ApolloClient<NormalizedCacheObject>;
 
 const createApolloClient = () => {
   return new ApolloClient({
-    ssrMode: typeof window === 'undefined',
+    ssrMode: isSsr,
     link: new HttpLink({ uri }),
     cache: new InMemoryCache(),
   });
+};
+
+export const initApollo = (
+  initialState: NormalizedCacheObject | null = null
+) => {
+  const client = apolloClient || createApolloClient();
+
+  if (initialState) {
+    client.cache.restore({
+      ...client.extract(),
+      ...initialState,
+    });
+  }
+
+  if (isSsr) {
+    return client;
+  }
+
+  if (!apolloClient) {
+    apolloClient = client;
+  }
+
+  return client;
 };
