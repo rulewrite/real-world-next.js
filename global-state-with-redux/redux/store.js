c4c7c0970600d19b163921bb9623a76ddfa54112
rulewrite <rulewrite@gmail.com>
2023-07-30 15:10:59 +0900

    feat: 리덕스 스토어 초기화 함수 정의

--- global-state-with-redux/redux/store.js
+++ global-state-with-redux/redux/store.js
@@ -1,3 +1,6 @@
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 const initialState = {};
 
 const reducer = (state = initialState, action) => {
@@ -28,4 +31,37 @@ const reducer = (state = initialState, action) => {
       return state;
     }
   }
+};
+
+const initStore = (preloadedState = initialState) => {
+  return createStore(
+    reducer,
+    preloadedState,
+    composeWithDevTools(applyMiddleware())
+  );
+};
+
+let store;
+
+export const initializeStore = (preloadedState) => {
+  let _store = store ?? initStore(preloadedState);
+
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    });
+
+    store = undefined;
+  }
+
+  // Redux를 서버 사이드에서 초기화하는 경우 _store를 반환
+  if (typeof windwow === 'undefined') {
+    return _store;
+  }
+
+  if (!store) {
+    store = _store;
+  }
+  return _store;
 };
