11327b2cef878a579e5f9e1e5c67d77562364b7c
rulewrite <rulewrite@gmail.com>
2023-07-30 14:17:50 +0900

    feat: 장바구니 컨텐스트 추가 및 바인딩

--- global-state-with-context/pages/_app.js
+++ global-state-with-context/pages/_app.js
@@ -1,16 +1,25 @@
 import Head from 'next/head';
+import { useState } from 'react';
 import Navbar from '../components/Navbar';
+import ShoppingCartContext from '../components/context/cartContext';
 
 function MyApp({ Component, pageProps }) {
+  const [items, setItems] = useState({});
+
   return (
     <>
       <Head>
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
       </Head>
-      <Navbar />
-      <div className="w-9/12 m-auto pt-10">
-        <Component {...pageProps} />
-      </div>
+      <ShoppingCartContext.Provider value={{ items, setItems }}>
+        <Navbar />
+        <div className="w-9/12 m-auto pt-10">
+          <Component {...pageProps} />
+        </div>
+      </ShoppingCartContext.Provider>
     </>
   );
 }
