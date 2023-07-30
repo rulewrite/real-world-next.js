8b1aaf1a12006449faf993e45266428833f1b3d1
rulewrite <rulewrite@gmail.com>
2023-07-30 16:23:22 +0900

    feat: CSS Modules로 스타일링

--- style-with-css-module/src/pages/index.tsx
+++ style-with-css-module/src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Home.module.css';
 export default function Home() {
   return (
     <div className={styles.homepage}>
-      <h1>Welcome to the CSS Modules example</h1>
+      <h1 className={styles.title}>Welcome to the CSS Modules example</h1>
     </div>
   );
 }
