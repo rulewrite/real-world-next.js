dccfbe0267ab31432b6adf38647d521df7867497
rulewrite <rulewrite@gmail.com>
2023-07-30 16:36:48 +0900

    feat: :global 키워드로 전역 스타일 지정 (동작안함)

--- style-with-css-module/src/pages/index.tsx
+++ style-with-css-module/src/pages/index.tsx
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <div className={styles.homepage}>
       <h1 className={styles.title}>Welcome to the CSS Modules example</h1>
+      <div>
+        <p className="highlight">hello</p>
+        <p className="highlight">hello</p>
+        <p className="highlight">hello</p>
+        <p className="highlight">hello</p>
+      </div>
     </div>
   );
 }
