cd66986b26f4a150c8507ba17fd0886c90309e0c
rulewrite <rulewrite@gmail.com>
2023-08-07 12:35:15 +0900

    feat: scss @extend 키워드 사용

--- style-with-sass/src/pages/index.tsx
+++ style-with-sass/src/pages/index.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div className={styles.homepage}>
       <h1>Welcome to the CSS Modules example</h1>
+
+      <button className={styles['button-default']}>defulat</button>
+      <button className={styles['button-success']}>success</button>
+      <button className={styles['button-danger']}>danger</button>
     </div>
   );
 }
