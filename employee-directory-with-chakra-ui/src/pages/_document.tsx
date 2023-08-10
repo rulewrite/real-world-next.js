f6f3b446075ef9efa03a29a3539aa3a5c6f734dc
rulewrite <rulewrite@gmail.com>
2023-08-10 21:23:43 +0900

    feat: 라이트, 다크 테마 적용

--- employee-directory-with-chakra-ui/src/pages/_document.tsx
+++ employee-directory-with-chakra-ui/src/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { ColorModeScript, extendTheme } from '@chakra-ui/react';
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const config = {
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({
+  config,
+});
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- employee-directory-with-chakra-ui/src/pages/index.tsx
+++ employee-directory-with-chakra-ui/src/pages/index.tsx
@@ -1,12 +1,21 @@
-import { Button, VStack } from '@chakra-ui/react';
+import { Button, Text, VStack, useColorMode } from '@chakra-ui/react';
 
 export default function Home() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <VStack padding="10">
-      <Button backgroundColor="brand.100">brand.100</Button>
-      <Button backgroundColor="brand.200">brand.200</Button>
-      <Button backgroundColor="brand.300">brand.300</Button>
-      <Button backgroundColor="brand.400">brand.400</Button>
+      <Text fontSize="4xl" fontWeight="bold" as="h1">
+        Chakra UI
+      </Text>
+
+      <Text fontSize="2xl" fontWeight="semibold" as="h2">
+        Rendering in {colorMode} mode
+      </Text>
+
+      <Button aria-label="UI Theme" onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? 'dark' : 'light'} mode
+      </Button>
     </VStack>
   );
 }
