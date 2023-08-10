ab25cd2b1176a5bab38e9db59391e9066e317be8
rulewrite <rulewrite@gmail.com>
2023-08-10 21:14:56 +0900

    feat: theme 확장

--- employee-directory-with-chakra-ui/src/pages/_app.tsx
+++ employee-directory-with-chakra-ui/src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import '@/styles/globals.css';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+
+const customTheme = extendTheme({
+  colors: {
+    brand: {
+      100: '#ffebee',
+      200: '#e57373',
+      300: '#f44336',
+      400: '#e53935',
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
