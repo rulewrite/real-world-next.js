ac902649ff4bf82be2bac0b5c1a2f12093ed1277
rulewrite <rulewrite@gmail.com>
2023-08-10 21:17:51 +0900

    feat: 테마 컬러 적용

--- employee-directory-with-chakra-ui/src/pages/index.tsx
+++ employee-directory-with-chakra-ui/src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { Button, VStack } from '@chakra-ui/react';
+
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <VStack padding="10">
+      <Button backgroundColor="brand.100">brand.100</Button>
+      <Button backgroundColor="brand.200">brand.200</Button>
+      <Button backgroundColor="brand.300">brand.300</Button>
+      <Button backgroundColor="brand.400">brand.400</Button>
+    </VStack>
+  );
 }
