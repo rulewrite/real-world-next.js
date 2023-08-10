2be0f001062b0d110e942326abe163a474239c2c
rulewrite <rulewrite@gmail.com>
2023-08-10 21:29:55 +0900

    feat: 테마 제거 및 탑바 컴포넌트 생성

--- employee-directory-with-chakra-ui/src/pages/_app.tsx
+++ employee-directory-with-chakra-ui/src/pages/_app.tsx
@@ -1,22 +1,15 @@
+import TopBar from '@/components/TopBar';
 import '@/styles/globals.css';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
-
-const customTheme = extendTheme({
-  colors: {
-    brand: {
-      100: '#ffebee',
-      200: '#e57373',
-      300: '#f44336',
-      400: '#e53935',
-    },
-  },
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={customTheme}>
-      <Component {...pageProps} />
+    <ChakraProvider>
+      <TopBar />
+      <Box maxWidth="container.xl" margin="auto">
+        <Component {...pageProps} />
+      </Box>
     </ChakraProvider>
   );
 }
