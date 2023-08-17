818f754c6bb5caa37006230e984d80a66abd4e10
rulewrite <rulewrite@gmail.com>
2023-08-17 23:57:34 +0900

    feat: Next.js 연결 제거 및 더미 서버 기능으로 홈페이지 및 가짜 API 추가

--- custom-server-with-express-js/index.ts
+++ custom-server-with-express-js/index.ts
@@ -1,23 +1,15 @@
 import express from 'express';
-import next from 'next';
-import { parse } from 'url';
-
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
 
 async function main() {
   try {
-    // Next.js 앱 실행할 준비가 될 때까지 대기
-    await app.prepare();
-
-    // Next.js를 대신하여 요청을 처리할 handle
-    const handle = app.getRequestHandler();
     const server = express();
 
     server
-      .get('*', (request, response) => {
-        const url = parse(request.url, true);
-        handle(request, response, url);
+      .get('/', (_, response) => {
+        response.send('Hello world!');
+      })
+      .get('/api/greet', (request, response) => {
+        response.json({ name: request?.query?.name ?? 'unknown' });
       })
       .listen(3000, () => {
         console.log('server ready');
