0f3ca6cd28fe5a85f4390a37eb05b0068c4bd10e
rulewrite <rulewrite@gmail.com>
2023-08-18 01:19:51 +0900

    feat: contacts 페이지 추가

--- custom-server-with-fastify/index.js
+++ custom-server-with-fastify/index.js
@@ -4,6 +4,9 @@ fastify.register(require('fastify-nextjs')).after(() => {
   fastify.next('/');
   fastify.next('/about');
   fastify.next('/greet/:user');
+  fastify.get('/contacts', (request, reply) => {
+    reply.type('html').send('<h1>Contacts page</h1>');
+  });
 });
 
 fastify.listen(3000, () => {
