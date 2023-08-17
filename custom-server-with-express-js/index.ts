75d1bc70de27127f5fd747eb8f6648b910c5c34c
rulewrite <rulewrite@gmail.com>
2023-08-18 00:30:57 +0900

    feat: Next.js about 페이지 추가

--- custom-server-with-express-js/index.ts
+++ custom-server-with-express-js/index.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import next from 'next';
+import { parse } from 'url';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
 
 async function main() {
   try {
+    await app.prepare();
+
     const server = express();
 
     server
       .get('/', (_, response) => {
         response.send('Hello world!');
+      })
+      .get('/about', (request, response) => {
+        const { query } = parse(request.url, true);
+        app.render(request, response, '/about', query);
       })
       .get('/api/greet', (request, response) => {
         response.json({ name: request?.query?.name ?? 'unknown' });
