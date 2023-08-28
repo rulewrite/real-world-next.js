7711f08f3760213712278e8029dab57f89636437
rulewrite <rulewrite@gmail.com>
2023-08-28 20:24:40 +0900

    test: 컴포넌트 요약 텍스트가 100자를 초과할 수 없다

--- test-with-jest/components/ArticleCard/tests/index.test.js
+++ test-with-jest/components/ArticleCard/tests/index.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { cutTextToLength } from '../../../utils';
 import ArticleCard from '../index';
 import { article } from './mock';
 
@@ -11,5 +12,11 @@ describe('ArticleCard', () => {
     const component = render(<ArticleCard {...article} />);
     const link = component.getByRole('link').getAttribute('href');
     expect(link).toBe('/articles/healthy-summer-meloncarrot-soup-u12w3o0d');
+  });
+
+  test('생성된 요약은 100자를 초과할 수 없다', async () => {
+    render(<ArticleCard {...article} />);
+    const summary = screen.getByText(cutTextToLength(article.body, 100));
+    expect(summary).toBeDefined();
   });
 });
