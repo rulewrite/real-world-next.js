3ee5277af1ed7cafc91e32437664246249e42ceb
rulewrite <rulewrite@gmail.com>
2023-08-28 20:13:36 +0900

    test: ArticleCard 링크

---
+++ test-with-jest/components/ArticleCard/tests/index.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import ArticleCard from '../index';
+import { article } from './mock';
+
+describe('ArticleCard', () => {
+  test('생성된 링크가 올바른 형식인지', () => {
+    const component = render(<ArticleCard {...article} />);
+    const link = component.getByRole('link').getAttribute('href');
+    expect(link).toBe('/articles/healthy-summer-meloncarrot-soup-u12w3o0d');
+  });
+});
