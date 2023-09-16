9e1438448686f3f08a402d07772da2178db25b6f
rulewrite <rulewrite@gmail.com>
2023-09-16 18:00:25 +0900

    feat: 이메일, 비밀번호 인증 절차 추가

--- with-custom-auth/src/pages/api/login.ts
+++ with-custom-auth/src/pages/api/login.ts
@@ -13,6 +13,30 @@ const login = (req: NextApiRequest, res: NextApiResponse) => {
       error: 'Missing required params',
     });
   }
+
+  const user = authenticateUser(email, password);
+  if (!user) {
+    return res.status(401).json({
+      error: 'Wrong email of password',
+    });
+  }
+
+  return res.json({ user });
 };
+
+function authenticateUser(email: string, password: string) {
+  const validEmail = 'johndoe@somecompany.com';
+  const validPassword = 'strongpassword';
+
+  if (email === validEmail && password === validPassword) {
+    return {
+      id: 'f678f078-fcfe-43ca-9d20-e8c9a95209b6',
+      name: 'John Doe',
+      email: 'johndoe@somecompany.com',
+    };
+  }
+
+  return null;
+}
 
 export default login;
