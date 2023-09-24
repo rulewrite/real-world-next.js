3756213f4b748cd73981b99011bcc95e1fafe77c
rulewrite <rulewrite@gmail.com>
2023-09-25 00:33:41 +0900

    feat: 인증 훅 추가 및 보호 페이지 진입간 인증 추가

--- with-custom-auth/src/pages/protected-route.tsx
+++ with-custom-auth/src/pages/protected-route.tsx
@@ -1,10 +1,26 @@
+import { useAuth } from '@/lib/hooks/auth';
+import { useRouter } from 'next/router';
 import styles from '../styles/app.module.css';
 
 export default function ProtectedRoute() {
+  const router = useRouter();
+  const { loading, error, loggedIn } = useAuth();
+
+  if (!loading && !loggedIn) {
+    router.push('/login');
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Protected Route</h1>
-      <p>You can&apos;t see me if not logged-in!</p>
+      {loading && <p>Loading...</p>}
+      {error && <p>An error occurred</p>}
+
+      {loggedIn && (
+        <>
+          <h1>Protected Route</h1>
+          <p>You can&apos;t see me if not logged-in!</p>
+        </>
+      )}
     </div>
   );
 }
