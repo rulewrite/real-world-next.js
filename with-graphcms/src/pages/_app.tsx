fe5dbca2814eb5263948da279e57033a8cbb8781
timothy.run <timothy.run@kakaocorp.com>
2023-10-24 21:05:18 +0900

    feat: 네비게이션바 컴포넌트 추가 및 렌더링

--- with-graphcms/src/pages/_app.tsx
+++ with-graphcms/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import NavBar from '@/components/NavBar';
 import '@/styles/globals.css';
 import { Box, ChakraProvider, Flex } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
@@ -6,6 +7,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Flex w="full" minH="100vh" bgColor="gray.100">
+        <NavBar />
+
         <Box maxW="70vw" m="auto">
           <Component {...pageProps} />
         </Box>
