4f8af6284fa5dae38ee88a2b7e471dabcbfdd6a2
timothy.run <timothy.run@kakaocorp.com>
2023-10-24 19:51:25 +0900

    feat: index 정적 페이지로 전환 및 상품 가져오기

--- with-graphcms/src/pages/index.tsx
+++ with-graphcms/src/pages/index.tsx
@@ -1,3 +1,16 @@
+import graphQLClient from '@/lib/graphql';
+import getAllProducts from '@/lib/graphql/queries/getAllProducts';
+
+export const getStaticProps = async () => {
+  const { products } = await graphQLClient.request(getAllProducts);
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
+
 export default function Home() {
   return <>hello world</>;
 }
