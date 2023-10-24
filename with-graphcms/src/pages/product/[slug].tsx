45ff3e56b18c2e7d112c03e008f6c7d7cac6eacc
timothy.run <timothy.run@kakaocorp.com>
2023-10-24 20:27:12 +0900

    feat: 상품 별로 정적 페이지 생성

--- with-graphcms/src/pages/product/[slug].tsx
+++ with-graphcms/src/pages/product/[slug].tsx
@@ -1,3 +1,33 @@
+import graphQLClient from '@/lib/graphql';
+import getAllProducts from '@/lib/graphql/queries/getAllProducts';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+// 각 상품별로 정적 페이지 생성
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { products } = await graphQLClient.request<{
+    products: Array<Product>;
+  }>(getAllProducts);
+
+  const paths = products.map((product) => {
+    return {
+      params: {
+        slug: product.slug,
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
 const ProductPage: React.FC = () => {
   return null;
 };
