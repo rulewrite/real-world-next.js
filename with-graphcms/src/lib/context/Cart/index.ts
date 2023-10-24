a25453b421703eabfa06f9ddaf7664e82500c4c7
timothy.run <timothy.run@kakaocorp.com>
2023-10-24 21:13:48 +0900

    feat: 장바구니 컨텍스트 전역으로 바인딩

--- with-graphcms/src/lib/context/Cart/index.ts
+++ with-graphcms/src/lib/context/Cart/index.ts
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 
 const CartContext = createContext({
   items: {},
-  setItems: () => {},
+  setItems: (items: {}) => {},
 });
 
 export default CartContext;
--- with-graphcms/src/pages/_app.tsx
+++ with-graphcms/src/pages/_app.tsx
@@ -1,18 +1,24 @@
 import NavBar from '@/components/NavBar';
+import CartContext from '@/lib/context/Cart';
 import '@/styles/globals.css';
 import { Box, ChakraProvider, Flex } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [items, setItems] = useState({});
+
   return (
     <ChakraProvider>
-      <Flex w="full" minH="100vh" bgColor="gray.100">
-        <NavBar />
+      <CartContext.Provider value={{ items, setItems }}>
+        <Flex w="full" minH="100vh" bgColor="gray.100">
+          <NavBar />
 
-        <Box maxW="70vw" m="auto">
-          <Component {...pageProps} />
-        </Box>
-      </Flex>
+          <Box maxW="70vw" m="auto">
+            <Component {...pageProps} />
+          </Box>
+        </Flex>
+      </CartContext.Provider>
     </ChakraProvider>
   );
 }
