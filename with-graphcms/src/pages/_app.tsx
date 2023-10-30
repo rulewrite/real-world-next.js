3da1ff4ab219f7a7ca1e5d9f4473caf5943b2fa7
timothy.run <timothy.run@kakaocorp.com>
2023-10-30 23:57:40 +0900

    chore: 테스트용 임시 코드

--- with-graphcms/src/pages/_app.tsx
+++ with-graphcms/src/pages/_app.tsx
@@ -6,7 +6,15 @@ import type { AppProps } from 'next/app';
 import { useState } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [items, setItems] = useState({});
+  const [items, setItems] = useState({
+    ckdu4a79c0h0g0158pqypj0dp: 3,
+    ckdu4bmyg0h1f0102jk0mwn2g: 2,
+    ckdu44mn40gxh010405uwgbtw: 2,
+    ckdu48unc0gzq0158mbzvyzg3: 6,
+    ckdu49mfc0h070102jgprxnj0: 5,
+  } as {
+    [productId: string]: number;
+  });
 
   return (
     <ChakraProvider>
