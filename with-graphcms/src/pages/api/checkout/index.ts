1959d31f8389f01056d4cd815366ece865a85f2c
timothy.run <timothy.run@kakaocorp.com>
2023-10-30 22:59:58 +0900

    feat: 배송 가능 국가 옵션 추가 및 타입 정의

--- with-graphcms/src/pages/api/checkout/index.ts
+++ with-graphcms/src/pages/api/checkout/index.ts
@@ -4,6 +4,12 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '');
 
+// 배송 가능한 국가
+const shipping_address_collection: Stripe.Checkout.SessionCreateParams.ShippingAddressCollection =
+  {
+    allowed_countries: ['IT', 'US'],
+  };
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -12,31 +18,33 @@ export default async function handler(
 
   const { products } = await getProductDetailsById(Object.keys(items));
 
-  const line_items = products.map((product) => {
-    return {
-      // 사용자가 결제 과정에서 수량을 변경할 수 있도록
-      adjustable_quantity: {
-        enabled: true,
-        minimum: 1,
-      },
-      price_data: {
-        // 원하는 화폐 단위 지정
-        currency: 'EUR',
-        product_data: {
-          name: product.name,
-          images: product.images.map((image) => image.url),
+  const line_items: Array<Stripe.Checkout.SessionCreateParams.LineItem> =
+    products.map((product) => {
+      return {
+        // 사용자가 결제 과정에서 수량을 변경할 수 있도록
+        adjustable_quantity: {
+          enabled: true,
+          minimum: 1,
         },
-        unit_amount: product.price,
-      },
-      quantity: items[product.id],
-    };
-  });
+        price_data: {
+          // 원하는 화폐 단위 지정
+          currency: 'EUR',
+          product_data: {
+            name: product.name,
+            images: product.images.map((image) => image.url),
+          },
+          unit_amount: product.price,
+        },
+        quantity: items[product.id],
+      };
+    });
 
   const session = await stripe.checkout.sessions.create({
     // 'subscription' 또는 'setup'을 지정할 수 있다.
     mode: 'payment',
     line_items,
     payment_method_types: ['card', 'sepa_debit'],
+    shipping_address_collection,
     // .env.* 파일에서 지정
     success_url: `${process.env.URL}/success`,
     cancel_url: `${process.env.URL}/cancel`,
