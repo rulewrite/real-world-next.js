993be143832e146dd21a27e0d08e0a2d539616ff
timothy.run <timothy.run@kakaocorp.com>
2023-10-30 22:10:50 +0900

    feat: 장바구니 아이템 렌더링

--- with-graphcms/src/pages/cart.tsx
+++ with-graphcms/src/pages/cart.tsx
@@ -1,6 +1,7 @@
 import CartContext from '@/lib/context/Cart';
 import getProductsById from '@/lib/graphql/queries/getProductsById';
-import { Box, Divider, Text } from '@chakra-ui/react';
+import { Box, Button, Divider, Flex, Text } from '@chakra-ui/react';
+import Link from 'next/link';
 import { useContext, useEffect, useState } from 'react';
 
 const Cart: React.FC = () => {
@@ -24,6 +25,26 @@ const Cart: React.FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [hasProducts, JSON.stringify(ids)]);
 
+  const viewProducts = products.map((product) => {
+    return {
+      ...product,
+      viewPrice: product.price * (items[product.id] / 100),
+    };
+  });
+
+  const getTotal = () => {
+    if (!viewProducts.length) {
+      return 0;
+    }
+
+    return viewProducts
+      .map((product) => {
+        return product.viewPrice;
+      })
+      .reduce((x, y) => x + y)
+      .toFixed(2);
+  };
+
   return (
     <Box
       rounded={'xl'}
@@ -39,7 +60,48 @@ const Cart: React.FC = () => {
       <Divider my={10} />
 
       <Box>
-        <Text>The cart is empty.</Text>
+        {!hasProducts ? (
+          <Text>The cart is empty.</Text>
+        ) : (
+          <>
+            {viewProducts.map((product) => {
+              return (
+                <Flex key={product.id} justifyContent={'space-between'} mb={4}>
+                  <Box>
+                    <Link href={`/product/${product.slug}`} passHref>
+                      <Text
+                        fontWeight={'bold'}
+                        _hover={{
+                          textDecoration: 'underline',
+                          color: 'blue.500',
+                        }}
+                      >
+                        {product.name}
+
+                        <Text as={'span'} color={'gray.500'}>
+                          {' '}
+                          x{items[product.id]}
+                        </Text>
+                      </Text>
+                    </Link>
+                  </Box>
+
+                  <Box>{product.viewPrice.toFixed(2)}</Box>
+                </Flex>
+              );
+            })}
+
+            <Divider my={10} />
+
+            <Flex alignItems={'center'} justifyContent={'space-between'}>
+              <Text fontSize={'xl'} fontWeight={'bold'}>
+                Total: {getTotal()}
+              </Text>
+
+              <Button colorScheme="blue">Pay now</Button>
+            </Flex>
+          </>
+        )}
       </Box>
     </Box>
   );
